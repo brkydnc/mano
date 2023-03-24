32cc1b6d9e180a11c68fe10c140511971ac22fcf
Berkay Dinç <berkayydinc@gmail.com>
2023-03-24 11:56:06 +0300

    Implement assembler base

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, ChangeEvent } from 'react';
+import parse from './assembler/Parser';
+
+const PROGRAM = " ORG	100\n lda     cnt\n sza\n bun     wrk\n hlt\n wrk,    lda     spt\n sta     ptr\n\n lop,    lda     ptr i\n isz     ptr\n bw,     sko\n bun     bw\n out\n isz     cnt\n bun     lop\n hlt\n\n cnt,    dec     -5\n\n ptr,    dec     0\n \n str,    dec     67\n dec     65\n dec     80\n dec     82\n dec     75\n END"
+
+console.log(parse(PROGRAM));
 
 function App() {
+  const [source, setSource] = useState(PROGRAM);
+  const handler = (e: any) => {
+      e.preventDefault();
+      setSource(e.target.value);
+      console.clear();
+      console.log(parse(e.target.value));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <textarea value={source} onChange={handler}>
+      </textarea>
+    </main>
   );
 }
 
---
+++ src/assembler/Parser.ts
@@ -0,0 +1,196 @@
+type Result<T, E> = { ok: true, value: T } | { ok: false, error: E }
+
+const Ok = <T>(value: T): Result<T, never> => ({ ok: true, value });
+const Err = <E>(error: E): Result<never, E> => ({ ok: false, error });
+
+type Error = {
+    at: number,
+    cause: Cause,
+}
+
+type ParseResult<T> = Result<T, Error>;
+
+enum Cause {
+    InvalidLabel                = "Invalid label name, expected at least 1 letter, followed by at most 2 alphanumerics",
+    EmptyContent                = "Expected an instruction or a directive after the label",
+    TooManyMRIOperands          = "Too many number of memory reference instruction operands, expected at most 2",
+    TooManyNonMRIOperands       = "Non-memory reference instrucitons expect no operands.",
+    TooManyDirectiveOperands    = "Too many number of directive operands, expected only 1",
+    UnexpectedOperand           = "Unexpected operand, expected none",
+    UnrecognizedOperation       = "Unrecognized operation, expected an instruction or directive",
+    InvalidAddress              = "Invalid address, expected a label name",
+    InvalidHexadecimal          = "Invalid numeral, expected an unsigned hexadecimal value",
+    InvalidDecimal              = "Invalid numeral, expected a signed decimal value",
+    NoAddress                   = "Expected address operand, found none",
+    NoNumeral                   = "Expected a numeral operand",
+    InvalidIndirectionSymbol    = "Invalid indirection symbol, expected 'I' or none",
+}
+
+const Pattern = {
+    Comment: /\/.*/g,
+    Label: /^[A-Z][0-9A-Z]{0,2}$/,
+    Token: /\S+/g,
+    Blank: /^\s*$/,
+    Hexadecimal: /^[0-9A-F]+$/,
+    Decimal: /^(-|\+)?[0-9]+$/,
+}
+
+type MemoryReferenceInstruction = {
+    instruction: true,
+    op: string,
+    address: string,
+    indirect: boolean
+    label?: string,
+}
+
+type NonMemoryReferenceInstruction = {
+    instruction: true,
+    op: string,
+    label?: string,
+}
+
+type Directive = {
+    instruction: false,
+    name: "END",
+} | {
+    instruction: false,
+    name: "ORG",
+    numeral: number,
+} | {
+    instruction: false,
+    name: "DEC" | "HEX",
+    numeral: number,
+    label?: string,
+} 
+
+type Instruction = MemoryReferenceInstruction | NonMemoryReferenceInstruction;
+type Statement =  Instruction | Directive;
+type TranslationUnit = Statement[];
+
+const DIR = ["ORG", "END", "DEC", "HEX"];
+const IO  = ["INP", "OUT", "SKI", "SKO", "ION", "IOF"];
+const MRI = ["AND", "ADD", "LDA", "STA", "BUN", "BSA", "ISZ"];
+const RRI = ["CLA", "CLE", "CMA", "CME", "CIR", "CIL", "INC", "SPA", "SNA",
+             "SZA", "SZE", "HLT"];
+
+const isMemoryReferenceInstruction = (token: string): boolean => MRI.includes(token);
+const isRegisterReferenceInstruction = (token: string): boolean => RRI.includes(token);
+const isIOInstruction = (token: string): boolean => IO.includes(token);
+const isDirective = (token: string): boolean => DIR.includes(token);
+
+const isLabel = (str: string): boolean => Boolean(str.match(Pattern.Label));
+const isIndirectionSymbol = (str: string): boolean => str === "I";
+
+const removeComments = (source: string): string => source.replaceAll(Pattern.Comment, "");
+
+const parse = (input: string): ParseResult<TranslationUnit> => {
+    const upperCaseInput = input.toUpperCase();
+    const assembly = removeComments(upperCaseInput);
+    const lines = assembly.split('\n');
+    const statements: TranslationUnit = [];
+
+    for (const [index, line] of lines.entries()) {
+        // Skip the line if its blank (only whitespace).
+        if (line.match(Pattern.Blank)) continue;
+
+        const at = index + 1;
+        let label, content = line;
+
+        // Check if the line contains a comma, that means we have a label.
+        const commaIndex = line.indexOf(",");
+
+        // If the comma exists, split the line into two, label and content.
+        if (-1 < commaIndex) {
+            content = line.slice(commaIndex + 1);
+            label = line.slice(0, commaIndex).trim();
+            if (!isLabel(label)) return Err({ at, cause: Cause.InvalidLabel });
+        }
+
+        let tokens = content.match(Pattern.Token);
+
+        // Guarantee that we have at least 1 token.
+        if (!tokens) return Err({ at, cause: Cause.EmptyContent });
+
+        const [op, ...operands] = tokens;
+
+        // Determine the type of the op token.
+        if (isMemoryReferenceInstruction(op)) {
+            const [address, indirection] = operands;
+            const indirect = Boolean(indirection);
+
+            if (!address)
+                return Err({ at, cause: Cause.NoAddress });
+            if (!isLabel(address))
+                return Err({ at, cause: Cause.InvalidAddress });
+            if (indirection && !isIndirectionSymbol(indirection)) 
+                return Err({ at, cause: Cause.InvalidIndirectionSymbol });
+            if (operands.length > 2)
+                return Err({ at, cause: Cause.TooManyMRIOperands });
+
+            statements.push({ instruction: true, op, address, indirect, label });
+        } else if (isRegisterReferenceInstruction(op) || isIOInstruction(op)) {
+            if (operands.length > 0)
+                return Err({ at, cause: Cause.TooManyNonMRIOperands });
+
+            statements.push({ instruction: true, op, label });
+        } else if (isDirective(op)) {
+            const [numeral] = operands;
+
+            if (operands.length > 1)
+                return Err({ at, cause: Cause.TooManyMRIOperands });
+
+            if (numeral) {
+                const decimal = parseInt(numeral, 10);
+                const hexadecimal = parseInt(numeral, 16);
+
+                if (op === "ORG") {
+                    if (!numeral.match(Pattern.Decimal))
+                        return Err({ at, cause: Cause.InvalidDecimal });
+
+                    statements.push({
+                        instruction: false,
+                        name: "ORG",
+                        numeral: decimal
+                    });
+                } else if (op === "HEX") {
+                    if (!numeral.match(Pattern.Hexadecimal))
+                        return Err({ at, cause: Cause.InvalidHexadecimal });
+
+                    statements.push({
+                        instruction: false,
+                        name: "HEX",
+                        numeral: hexadecimal,
+                        label
+                    });
+                } else if (op === "DEC") {
+                    if (!numeral.match(Pattern.Decimal))
+                        return Err({ at, cause: Cause.InvalidDecimal });
+
+                    statements.push({
+                        instruction: false,
+                        name: "DEC",
+                        numeral: decimal,
+                        label
+                    });
+                } else {
+                    return Err({ at, cause: Cause.UnexpectedOperand });
+                }
+            } else {
+                if (op !== "END")
+                    return Err({ at, cause: Cause.NoNumeral });
+
+                statements.push({
+                    instruction: false,
+                    name: "END",
+                });
+            }
+
+        } else {
+            return Err({ at, cause: Cause.UnrecognizedOperation });
+        }
+    }
+
+    return Ok(statements);
+}
+
+export default parse;
