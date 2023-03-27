13ea9b9d9ba75c5361765c64c34be8d7899a9cd3
Berkay Dinç <berkayydinc@gmail.com>
2023-03-27 13:54:00 +0300

    Implement `and_then`

--- src/assembler/result.ts
+++ src/assembler/result.ts
@@ -1,7 +1,6 @@
 export type Result<T, E> = { ok: true, value: T } | { ok: false, error: E }
 
-const Ok = <T>(value: T): Result<T, never> => ({ ok: true, value });
-const Err = <E>(error: E): Result<never, E> => ({ ok: false, error });
+export const Ok = <T>(value: T): Result<T, never> => ({ ok: true, value });
+export const Err = <E>(error: E): Result<never, E> => ({ ok: false, error });
 
-export { Ok, Err };
-
+export const and_then = <T, U, E>(r: Result<T, E>, f: (t: T) => Result<U, E>) => r.ok ? f(r.value) : r;
